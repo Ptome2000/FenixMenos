b376a48b70b3a8531de7c05dff73a6b408e367fa
Ptome2000 <pedro.m.tome16@outlook.com>
2024-04-24 15:41:35 +0100

    Skeleton Template created + Urls

--- FenixMenos/settings.py
+++ FenixMenos/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'FenixMenos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'Templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
