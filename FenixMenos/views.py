e4311a6add5b8eb5f2d30a07bee189a1b4578cc1
pipsromao <filiparomao97@gmail.com>
2024-05-05 23:42:56 +0100

    Path commit Parte Carrosel

--- FenixMenos/views.py
+++ FenixMenos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from FenixMenos import settings
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'MEDIA_URL': settings.MEDIA_URL})
 
 
 def login(request):
